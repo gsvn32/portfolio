882c015d239c6766782595bcbb86e8171bca56bf
Nikhil Gangisetty <gangisetty.nikhil@gmail.com>
2024-07-03 12:28:40 -0500

    added content, clicable div

--- src/App.tsx
+++ src/App.tsx
@@ -39,11 +39,12 @@ function App() {
       <header className="App-header">
 <nav aria-label="Main Navigation">
         <ul className="nav-list">
-            <li className="nav-item"><a href="#home" className="nav-link">Home</a></li>
-            <li className="nav-item"><a href="#about" className="nav-link">Contact</a></li>
-            <li className="nav-item"><a href="#services" className="nav-link">Experience</a></li>
-            <li className="nav-item"><a href="#contact" className="nav-link">Projects</a></li>
+            <li className="nav-item"><a href="/" className="nav-link">Home</a></li>
+            <li className="nav-item"><a href="#experience" className="nav-link">Experience</a></li>
+            <li className="nav-item"><a href="#project" className="nav-link">Projects</a></li>
             <li className="nav-item"><a href="#contact" className="nav-link">Design Info</a></li>
+            <li className="nav-item"><a href="#contact" className="nav-link">Contact</a></li>
+            <li className="nav-item"><a href="#address" className="nav-link">Address</a></li>
         </ul>
     </nav>
     <div className="profile-picture">
@@ -66,7 +67,16 @@ function App() {
         <span className='left-arrow'></span>
       </div>
     </div>
-      <div className="container right">
+    <div className="container right" id='project'>
+    <img src="./project.png" alt="project icon" />
+      <div className="content">
+        <h2>TravelDjango (Python/Django)</h2>
+       <small>2024: Python, Django, JavaScript, MongoDB/MySQL, API, Git, Weather API</small>  
+        <p>A travel planning application that streamlines itinerary creation for tourists. Users can search destinations, receive real-time weather information, and build customized routes with various transportation options. </p>
+        <span className='right-arrow'></span>
+      </div>
+    </div>
+      <div className="container left">
     <img src="./work.png" alt="work icon" />
       <div className="content">
         <h2>UMKC ACED</h2>
@@ -75,21 +85,31 @@ function App() {
         <span className='left-arrow'></span>
       </div>
     </div>
+     
+    <div className="container right">
+    <img src="./project.png" alt="project icon" />
+      <div className="content">
+        <h2>GameMaster: A gamified task manager built in Next.js.</h2>
+        <small>2023: Node.js, React, TypeScript, MongoDB, GitHub Actions,HTML,CSS</small>
+    <p>Developed a full stack application enhancing user productivity through gamification. Designed and implemented user-friendly interfaces and interactive features.Optimized backend and frontend performance to ensure seamless user experiences.</p>
+        <span className='right-arrow'></span>
+      </div>
+    </div>
     <div className="container left">
+    <img src="./project.png" alt="project icon" />
+      <div className="content">
+        <h2>CaptionIt - Cloud based AI Image Captioning web Application</h2>
+        <small>Pytorch, django, Azure</small>
+    <p>Generate Descriptive and meaningful descriptions for Images. Developed using pre trained ML model and python Django for web development on Azure.</p>
+         <span className='left-arrow'></span>
+      </div>
+    </div>
+    <div className="container right" id='experience'>
     <img src="./work.png" alt="work icon" />
       <div className="content">
         <h2>Honeywell</h2>
         <small>2019-2022</small>
         <p>As a Sr. Software Engineer at Honeywell (June 2019 – Dec 2022), I developed Single Page Applications (SPAs) using React, incorporating HTML and CSS to create dynamic and responsive user interfaces focusing on optimizing the user experience and performance, ensuring high-quality web applications.I led the development of MVC applications using Spring Boot and AWS, designed complex SQL queries, and implemented CI/CD pipelines with GitHub and Jenkins.</p>
-        <span className='left-arrow'></span>
-      </div>
-    </div>
-    <div className=" container right">
-    <img src="./work.png" alt="work icon" />
-      <div className="content">
-      <h2>Infosys Ltd.</h2>
-      <small>2018-2019</small>
-        <p>As a Software Developer Intern at Infosys, I utilized HTML and CSS along with JavaScript and AJAX to design and build a web application for Exam Monitor. This project involved creating intuitive and accessible web interfaces, enhancing the overall usability and functionality of the application.</p>
         <span className='right-arrow'></span>
       </div>
     </div>
@@ -104,31 +124,13 @@ function App() {
         <span className='left-arrow'></span>
       </div>
     </div>
-    <div className="container right">
-    <img src="./project.png" alt="project icon" />
+    <div className=" container right">
+    <img src="./work.png" alt="work icon" />
       <div className="content">
-        <h2>TravelDjango (Python/Django)</h2>
-       <small>2024: Python, Django, JavaScript, MongoDB/MySQL, API, Git, Weather API</small>  
-        <p>A travel planning application that streamlines itinerary creation for tourists. Users can search destinations, receive real-time weather information, and build customized routes with various transportation options. </p>
+      <h2>Infosys Ltd.</h2>
+      <small>2018-2019</small>
+        <p>As a Software Developer Intern at Infosys, I utilized HTML and CSS along with JavaScript and AJAX to design and build a web application for Exam Monitor. This project involved creating intuitive and accessible web interfaces, enhancing the overall usability and functionality of the application.</p>
         <span className='right-arrow'></span>
-      </div>
-    </div>
-    <div className="container left">
-    <img src="./project.png" alt="project icon" />
-      <div className="content">
-        <h2>GameMaster: A gamified task manager built in Next.js.</h2>
-        <small>2023: Node.js, React, TypeScript, MongoDB, GitHub Actions,HTML,CSS</small>
-    <p>Developed a full stack application enhancing user productivity through gamification. Designed and implemented user-friendly interfaces and interactive features.Optimized backend and frontend performance to ensure seamless user experiences.</p>
-        <span className='left-arrow'></span>
-      </div>
-    </div>
-    <div className="container right">
-    <img src="./project.png" alt="project icon" />
-      <div className="content">
-        <h2>CaptionIt - Cloud based AI Image Captioning web Application</h2>
-        <small>Pytorch, django, Azure</small>
-    <p>Generate Descriptive and meaningful descriptions for Images. Developed using pre trained ML model and python Django for web development on Azure.</p>
-         <span className='right-arrow'></span>
       </div>
     </div>
     <div className="container left">
@@ -143,23 +145,20 @@ function App() {
   </div>
       </main>
       <footer className="App-footer">
-        <div>
+        <div id='contact'>
         <h3>Contact</h3>
         <h5>Nikhil Gangisetty</h5>
         <a href="mailto:gangietty.nikhil@gmail.com" title="send email">gangietty.nikhil@gmail.com</a>
         <p>+1 913-709-9391</p>
         </div>
-        <div>
+        <div id='address'>
           <h3>Address</h3>
           <p>301 W Armour blvd</p>
           <p>Kansas City, MO</p>
         </div>
         <div>
-        <p className='copyright'>&copy; <span id="currentYear"></span> Nikhil Gangisetty. All rights reserved.</p>
-        </div>
-        <div>
-          <h4>Credits</h4>
-        <a href="https://www.flaticon.com/" title="icons" className='credit'>icons created by egorpolyakov - Flaticon</a>
+        <p className='copyright'>&copy; <span id="currentYear"></span> Nikhil Gangisetty. All rights reserved.| <a href="https://www.flaticon.com/" title="icons" className='credit'>Credits</a>
+        </p>
         </div>
         <script>
         document.getElementById('currentYear').textContent = new Date().getFullYear();
